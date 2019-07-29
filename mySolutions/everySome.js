99b2e2aec1069b87c34aa2218c0cd1bb3993c552
Jenesh <jenesh.napit@gmail.com>
2019-07-29 17:04:10 -0400

    some changes

--- mySolutions/everySome.js
+++ mySolutions/everySome.js
@@ -1,5 +1,26 @@
 module.exports = function checkUsersValid(goodUsers) {
     return function allUsersValid(submittedUsers) {
-        submittedUsers.every(user => user.some(goodUsers.id))
+    let count = 0;
+    submittedUsers.every(user => 
+        goodUsers.some(verifiedUsrs => verifiedUsrs.id === user.id)?
+            count += 1 : count += 0);
+        return count;
     }
 }
+
+// let arr1 = [
+//     {id: 1},
+//     {id: 2},
+//     {id: 3}
+// ]
+
+// let arr2 = [
+//     {id: 1},
+//     {id: 2},
+//     {id: 3},
+// ];
+
+// function check(arr, arr2) {
+//     console.log(arr2.every(ele => arr1.some(ele2 => ele2.id === ele.id)));
+// }
+// console.log(checkUsersValid(arr1)(arr2));
--- mySolutions/filter.js
+++ mySolutions/filter.js
@@ -1,5 +1,16 @@
-module.exports = function getShortMessages(messages) {
-    let arr = [];
-    messages.filter(obj => obj.message.length < 50 ? arr.push(obj.message) : false);
-    return arr;
+// module.exports = function getShortMessages(messages) {
+//     let arr = [];
+//     messages.filter(obj => obj.message.length < 50 ? arr.push(obj.message) : false);
+//     return arr;
+// }
+function shortString(txt) {
+    return txt.length < 50
 }
+function getMessage(obj) {
+    return obj.message
+}
+module.exports = function getShortMessages(message) {
+    return message.map(getMessage).filter(shortString);
+}
+
+// console.log(getShortMessages(["safds",'asdfasdfafds']))
