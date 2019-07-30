6da4abfabfa8a9a38d269be20aa64314e5fb8b1b
Jenesh <jenesh.napit@gmail.com>
2019-07-29 21:21:16 -0400

    more files

---
+++ mySolutions/reduce.js
@@ -0,0 +1,7 @@
+function countWords(inputWords) {
+    return inputWords.reduce((acc, item) => objectMe(item, acc), {});
+}
+function objectMe(item, acc) {
+    acc.
+}
+let words = ['Apple', 'Banana', 'Apple', 'Diruan', 'Diruan', 'Diruan',];
